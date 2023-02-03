a45a5a6bce2a0dddfa4f313510b7cec7313db816
Aleksei Nosikov <alex.nosikov@tenging.is>
2023-02-04 02:57:31 +0300

    feat: adds react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './index.scss'
-import { FC } from 'react';
-import Counter from './components/Counter';
+import { FC, Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 
 const App: FC = () => {
   return (
     <div className="app">
-      <Counter />
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/'} element={<MainPageAsync />} />
+          <Route path={'/about'} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const AboutPage: FC = () => {
+  return (
+    <div>
+      ABOUT PAGE
+    </div>
+  );
+};
+
+export default AboutPage;
